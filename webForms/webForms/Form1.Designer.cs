d28df2b0f35721d9c641ece89ffdb7c8dc517c3e
lgodi <50015869+lgodi@users.noreply.github.com>
2020-02-05 10:30:54 +0530

    Add files via upload

---
+++ webForms/webForms/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace webForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbluname = new System.Windows.Forms.Label();
+            this.lblpass = new System.Windows.Forms.Label();
+            this.txtuname = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.btninsert = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnclr = new System.Windows.Forms.Button();
+            this.lblid = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btndisp = new System.Windows.Forms.Button();
+            this.dgvdisp = new System.Windows.Forms.DataGridView();
+            this.dataSet1 = new System.Data.DataSet();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.listView1 = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvdisp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbluname
+            // 
+            this.lbluname.AutoSize = true;
+            this.lbluname.Location = new System.Drawing.Point(141, 73);
+            this.lbluname.Name = "lbluname";
+            this.lbluname.Size = new System.Drawing.Size(57, 13);
+            this.lbluname.TabIndex = 0;
+            this.lbluname.Text = "UserName";
+            // 
+            // lblpass
+            // 
+            this.lblpass.AutoSize = true;
+            this.lblpass.Location = new System.Drawing.Point(145, 106);
+            this.lblpass.Name = "lblpass";
+            this.lblpass.Size = new System.Drawing.Size(53, 13);
+            this.lblpass.TabIndex = 1;
+            this.lblpass.Text = "Password";
+            // 
+            // txtuname
+            // 
+            this.txtuname.Location = new System.Drawing.Point(221, 66);
+            this.txtuname.Name = "txtuname";
+            this.txtuname.Size = new System.Drawing.Size(100, 20);
+            this.txtuname.TabIndex = 2;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(221, 99);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(100, 20);
+            this.txtpass.TabIndex = 3;
+            // 
+            // btninsert
+            // 
+            this.btninsert.Location = new System.Drawing.Point(127, 161);
+            this.btninsert.Name = "btninsert";
+            this.btninsert.Size = new System.Drawing.Size(75, 23);
+            this.btninsert.TabIndex = 4;
+            this.btninsert.Text = "Insert";
+            this.btninsert.UseVisualStyleBackColor = true;
+            this.btninsert.Click += new System.EventHandler(this.btninsert_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(237, 161);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(75, 23);
+            this.btnupdate.TabIndex = 5;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(349, 161);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 23);
+            this.btndelete.TabIndex = 6;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnclr
+            // 
+            this.btnclr.Location = new System.Drawing.Point(454, 161);
+            this.btnclr.Name = "btnclr";
+            this.btnclr.Size = new System.Drawing.Size(75, 23);
+            this.btnclr.TabIndex = 7;
+            this.btnclr.Text = "Clear";
+            this.btnclr.UseVisualStyleBackColor = true;
+            this.btnclr.Click += new System.EventHandler(this.btnclr_Click);
+            // 
+            // lblid
+            // 
+            this.lblid.AutoSize = true;
+            this.lblid.Location = new System.Drawing.Point(145, 38);
+            this.lblid.Name = "lblid";
+            this.lblid.Size = new System.Drawing.Size(16, 13);
+            this.lblid.TabIndex = 8;
+            this.lblid.Text = "Id";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(221, 22);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(100, 20);
+            this.txtid.TabIndex = 9;
+            this.txtid.TextChanged += new System.EventHandler(this.txtid_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 260);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // btndisp
+            // 
+            this.btndisp.Location = new System.Drawing.Point(548, 161);
+            this.btndisp.Name = "btndisp";
+            this.btndisp.Size = new System.Drawing.Size(75, 23);
+            this.btndisp.TabIndex = 11;
+            this.btndisp.Text = "Display";
+            this.btndisp.UseVisualStyleBackColor = true;
+            this.btndisp.Click += new System.EventHandler(this.btndisp_Click);
+            // 
+            // dgvdisp
+            // 
+            this.dgvdisp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvdisp.Location = new System.Drawing.Point(516, 260);
+            this.dgvdisp.Name = "dgvdisp";
+            this.dgvdisp.Size = new System.Drawing.Size(240, 150);
+            this.dgvdisp.TabIndex = 12;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(303, 274);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 97);
+            this.listView1.TabIndex = 13;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.dgvdisp);
+            this.Controls.Add(this.btndisp);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.lblid);
+            this.Controls.Add(this.btnclr);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btninsert);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtuname);
+            this.Controls.Add(this.lblpass);
+            this.Controls.Add(this.lbluname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvdisp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbluname;
+        private System.Windows.Forms.Label lblpass;
+        private System.Windows.Forms.TextBox txtuname;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Button btninsert;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnclr;
+        private System.Windows.Forms.Label lblid;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btndisp;
+        private System.Windows.Forms.DataGridView dgvdisp;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ webForms/webForms/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace webForms
+{
+    public partial class Form1 : Form
+    {
+        SqlCommand cmd;
+        SqlConnection con;
+        SqlDataAdapter da;
+        SqlDataReader dr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Admin;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con.Open();
+        }
+
+        private void btninsert_Click(object sender, EventArgs e)
+        {
+            if(txtid.Text==string.Empty)
+            {
+                errorProvider1.SetError(txtid, "can't insert");
+                txtid.Focus();
+            }
+            else
+            {
+                String qry = "insert into login (id,uname,password)values('" + txtid.Text + "','" + txtuname.Text + "','" + txtpass.Text + "')";
+                cmd = new SqlCommand(qry, con);
+                int exe = cmd.ExecuteNonQuery();
+                if (exe > 0)
+                {
+                    MessageBox.Show("Inserted sucessfully");
+                    da = new SqlDataAdapter("select * from login", con);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            String qry="update login set uname=" + txtid.Text + "','" + txtuname.Text + "' where id='"+txtid.Text+"'";
+            cmd = new SqlCommand(qry, con);
+            int exe = cmd.ExecuteNonQuery();
+            if(exe>0)
+            {
+                MessageBox.Show("Data Updated sucessfully");
+                da = new SqlDataAdapter("select * from login", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            String qry = "delete * from login";
+            cmd = new SqlCommand(qry, con);
+            int exe = cmd.ExecuteNonQuery();
+            if(exe>0)
+            {
+                MessageBox.Show("Data deleted sucessfully");
+                da = new SqlDataAdapter("select * from login", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+        private void btndisp_Click(object sender, EventArgs e)
+        {
+            da = new SqlDataAdapter("select*from login", con);
+            DataSet ds = new DataSet();
+            da.Fill();
+            con.Close();
+            DataGridView dv = new DataGridView();
+
+        }
+
+        private void btnclr_Click(object sender, EventArgs e)
+        {
+            txtid.Text = "";
+            txtuname.Text = "";
+            txtpass.Text = "";
+        }
+
+        private void txtid_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
